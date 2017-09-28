ef4c1fe4d3d14e9b4394e29cafb58598e05eb736
Angelo Manganiello <angelo.mang@libero.it>
2017-09-28 18:14:56 +0200

    added configuration for index.html

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RestController
 public class DemoController {
 
-	@RequestMapping("/")
+	@RequestMapping("/welcome")
 	public String index() {
-		return "index";
+		return "welcome";
 	}
 
 }
