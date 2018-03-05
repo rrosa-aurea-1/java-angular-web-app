0ab2763339cbc15cae51b6891dd87546f4b0ab86
Angelo Manganiello <angelo.mang@libero.it>
2018-03-05 10:56:06 +0100

    added config for singlePageApp routing in config spring boot class

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,53 +1,14 @@
 package com.example.demo;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.event.EventListener;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class DemoApplication {
-
-	private static final Logger logger = LoggerFactory.getLogger(DemoApplication.class);
-
-	// value inserted in your application.properties server.port
-	@Value("${server.port}")
-	private String port;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 
 	}
-
-	@EventListener(ApplicationReadyEvent.class)
-	public void postConstruct() {
-		logger.info("-----OPEN BROWSER IN localhost:" + port);
-	}
-
-	@Bean
-	WebMvcConfigurer configurer() {
-		return new WebMvcConfigurerAdapter() {
-			@Override
-			public void addResourceHandlers(ResourceHandlerRegistry registry) {
-
-				// registry.addResourceHandler("assets/**/*.*").addResourceLocations("classpath:/static");
-
-			}
-
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**");
-			}
-
-		};
-	};
 
 }
--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -2,13 +2,15 @@ package com.example.demo;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class DemoController {
 
-	@RequestMapping(value = { "/{path:[^\\.]*}/**" })
+	@RequestMapping(value = { "/welcome" })
+	@ResponseBody
 	public String redirect() {
-		return "forward:/";
+		return "hello!";
 	}
-	
+
 }
