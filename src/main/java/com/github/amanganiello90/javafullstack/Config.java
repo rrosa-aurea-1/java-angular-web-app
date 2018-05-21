7d1381bab2fbf284139dfe802eeb343810b60b5a
Angelo Manganiello <angelo.mang@libero.it>
2018-05-21 12:57:19 +0200

    fix first integration with errorPage and web miss dependency

--- src/main/java/com/github/amanganiello90/javafullstack/Config.java
+++ src/main/java/com/github/amanganiello90/javafullstack/Config.java
@@ -6,14 +6,13 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.boot.web.servlet.ErrorPage;
+import org.springframework.boot.context.embedded.ErrorPage;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.event.EventListener;
 import org.springframework.http.HttpStatus;
 
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 /**
