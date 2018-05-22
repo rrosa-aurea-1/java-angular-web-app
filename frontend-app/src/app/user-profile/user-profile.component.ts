b05dcf62633b950f8a5ceeaa6801877526b8dae4
Angelo Manganiello <angelo.mang@libero.it>
2018-05-22 17:13:12 +0200

    initial user.service added

--- frontend-app/src/app/user-profile/user-profile.component.ts
+++ frontend-app/src/app/user-profile/user-profile.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from './user.service';
+import { Observable } from 'rxjs/Rx';
 
 @Component({
   selector: 'app-user-profile',
@@ -7,9 +9,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserProfileComponent implements OnInit {
 
-  constructor() { }
+  public users;
+
+  constructor(private _userService: UserService) { }
 
   ngOnInit() {
+
+    this.getUsers();
+  }
+
+  getUsers() {
+    this._userService.getUsers().subscribe(
+      data => { this.users = data },
+      err => console.error(err),
+      () => console.log('done loading users')
+    );
+  }
+
+  createUser() {
+    let user = { id: '1' };
+    this._userService.createUser(user).subscribe(
+      data => {
+        // refresh the list
+        this.getUsers();
+        return true;
+      },
+      error => {
+        console.error("Error saving user!");
+        return Observable.throw(error);
+      }
+    );
+  }
+
+  deleteUser(user: any) {
+    if (confirm("Are you sure you want to delete " + user.username + "?")) {
+      this._userService.deleteUser(user).subscribe(
+        data => {
+          // refresh the list
+          this.getUsers();
+          return true;
+        },
+        error => {
+          console.error("Error deleting user!");
+          return Observable.throw(error);
+        }
+      );
+    }
   }
 
 }
