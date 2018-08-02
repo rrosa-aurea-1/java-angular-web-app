0d8ab952bc248d0b20b74eca0b2ebb6eb738901a
Angelo Manganiello <angelo.mang@libero.it>
2018-08-02 10:04:46 +0200

    fix reload on electron-client project

--- electron-client/main.js
+++ electron-client/main.js
@@ -1,38 +1,66 @@
 const electron = require("electron"),
-  app = electron.app,
-  BrowserWindow = electron.BrowserWindow;
+app = electron.app,
+BrowserWindow = electron.BrowserWindow;
 
-require('electron-debug')({ enabled: true});
+require('electron-debug')({
+	enabled: true
+});
 let mainWindow;
+const fs = require('fs');
+let url = `${__dirname}/dist/index.html`;
+const globalShortcut = electron.globalShortcut;
 
 function createWindow() {
-  mainWindow = new BrowserWindow({
-    autoHideMenuBar: true,
-    width: 640,
-    height: 480,
-	webPreferences: {
-	nodeIntegration: false   }
-  });
-  mainWindow.loadURL(`file://${__dirname}/dist/index.html`);
-  //mainWindow.webContents.openDevTools();
-  mainWindow.on("closed", function() {
-    mainWindow = null;
-  });
+	mainWindow = new BrowserWindow({
+			autoHideMenuBar: true,
+			width: 640,
+			height: 480,
+			webPreferences: {
+				nodeIntegration: false
+			}
+		});
+	mainWindow.loadURL(url);
+	//mainWindow.webContents.openDevTools();
+	mainWindow.on("closed", function () {
+		mainWindow = null;
+	});
+
+	globalShortcut.register('f5', function () {
+		reloadPage();
+	});
+	globalShortcut.register('CommandOrControl+R', function () {
+		reloadPage();
+	});
+}
+
+function reloadPage() {
+	var currentURL = mainWindow.webContents.getURL();
+	let urlWithoutFileProtocol = currentURL.split('file:///');
+	currentURL = urlWithoutFileProtocol[1];
+	try {
+		if (fs.statSync(currentURL).isFile()) {
+			mainWindow.reload();
+		} else {
+			mainWindow.loadURL(url);
+		}
+	} catch (err) {
+		mainWindow.loadURL(url);
+	}
 }
 
 app.on("ready", createWindow);
-app.on("browser-window-created", function(e, window) {
-  window.setMenu(null);
+app.on("browser-window-created", function (e, window) {
+	window.setMenu(null);
 });
 
-app.on("window-all-closed", function() {
-  if (process.platform !== "darwin") {
-    app.quit();
-  }
+app.on("window-all-closed", function () {
+	if (process.platform !== "darwin") {
+		app.quit();
+	}
 });
 
-app.on("activate", function() {
-  if (mainWindow === null) {
-    createWindow();
-  }
+app.on("activate", function () {
+	if (mainWindow === null) {
+		createWindow();
+	}
 });
