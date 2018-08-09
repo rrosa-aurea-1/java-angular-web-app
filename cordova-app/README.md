3ff31726eeb61ca085b8579bbeb41104520b3269
Angelo Manganiello <angelo.mang@libero.it>
2018-08-09 19:53:11 +0200

    change plugin http cordova

--- cordova-app/README.md
+++ cordova-app/README.md
@@ -3,7 +3,7 @@ cordova cli 8.0.0
 I have run:
 - cordova create app com.amanganiello30.angular angularDashFullStack
 - cordova platform add android --> 7.0.0
-- cordova plugin add https://github.com/fullStackApp/cordova-plugin-local-webserver
+- cordova plugin add cordova-plugin-httpd
 - cordova plugin add cordova-sqlite-storage
 - add GRADLE_HOME system environment variable with final /bin and insert into PATH
 - I copied the server-cordova.js file into www together the dist app builded and inject as javascript script in index.html
--- cordova-app/server-cordova.js
+++ cordova-app/server-cordova.js
@@ -1,29 +1,24 @@
-
-
-// device APIs are available
-function onDeviceReady() {
-	webserver.onRequest(
-		function (request) {
-		console.log("This is the request: ", request);
-
-		webserver.sendResponse(
-			request.requestId, {
-			status: 200,
-			body: '<html>Hello World</html>',
-			headers: {
-				'Content-Type': 'text/html'
-			}
-		});
-	});
-
-	// Starts webserver with default port 8080
-	webserver.start();
-
-	//... after a long long time
-	// stop the server
-	//webserver.stop();
-}
-
 // Wait for device API libraries to load
 document.addEventListener("deviceready", onDeviceReady, false);
 
+// device APIs are available
+  function onDeviceReady() {
+        startServer("");
+    }
+
+    function startServer( wwwroot ) {
+    	
+    	    httpd.getURL(function(url){
+    	    	    httpd.startServer({
+    	    	    	'www_root' : wwwroot,
+    	    	    	'port' : 8080
+    	    	    }, function( url ){
+        	    		document.getElementById('url').innerHTML = "server is started: <a href='" + url + "' target='_blank'>" + url + "</a>";
+    	    	    }, function( error ){
+    	    	    	alert('failed to start server');
+    	    	    });
+    	    	
+    	    	
+    	    },function(){});
+    	
+    }
